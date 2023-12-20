4b6ff85c7d0e7e2fb28d1908d9eb36c4abf2c96b
ArttuBrotherus <44648394+ArttuBrotherus@users.noreply.github.com>
2023-12-20 18:22:13 +0200

    Chap. 05 ex. 12

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,248 +6,248 @@ import BlogForm from './components/blogViewSepa'
 import PropTypes from 'prop-types'
 
 const App = () => {
-  const [username, setUsername] = useState('') 
-  const [password, setPassword] = useState('')
-  const [user, setUser] = useState(null)
-  const [blogs, setBlogs] = useState([])
-  const [notification, setNotif] = useState('')
-  const [userAddedBlogs, setUsAdBlogs] = useState([])
+	const [username, setUsername] = useState('')
+	const [password, setPassword] = useState('')
+	const [user, setUser] = useState(null)
+	const [blogs, setBlogs] = useState([])
+	const [notification, setNotif] = useState('')
+	const [userAddedBlogs, setUsAdBlogs] = useState([])
 
-  const appRef = useRef()
+	const appRef = useRef()
 
-  const Notification = ({ message }) => {
+	const Notification = ({ message }) => {
 
-    const notifStyle = {
-      background: "lightgrey",
-      fontSize: "20px",
-      borderStyle: "solid",
-      borderRadius: "5px",
-      padding: "10px",
-      marginBottom: "10px"
-    }
+		const notifStyle = {
+			background: "lightgrey",
+			fontSize: "20px",
+			borderStyle: "solid",
+			borderRadius: "5px",
+			padding: "10px",
+			marginBottom: "10px"
+		}
 
-    if (message === '') {
-      return null
-    }
-  
-    return (
-      <div className='error' style={notifStyle}>
-        {message}
-      </div>
-    )
-  }
+		if (message === '') {
+			return null
+		}
 
-  Notification.propTypes = {
-    message: PropTypes.string.isRequired
-  }
+		return (
+			<div className='error' style={notifStyle}>
+				{message}
+			</div>
+		)
+	}
 
-  function logOut () {
-    window.localStorage.clear()
-    document.location.reload()
-  }
+	Notification.propTypes = {
+		message: PropTypes.string.isRequired
+	}
 
-  const eachButton = (blog) => {
-    return {
-      visible: false,
-      full: blog
-    }
-  }
+	function logOut () {
+		window.localStorage.clear()
+		document.location.reload()
+	}
 
-  const dataOfLogged = async () => {
-    const serverBlogs = await pt05blogSer.getAll()
-    const blogButtons = await serverBlogs.map(eachButton)
-    const sortedBlogs = blogButtons.sort((a, b) => a.full.likes - b.full.likes)
-    setBlogs(sortedBlogs)
-  }
+	const eachButton = (blog) => {
+		return {
+			visible: false,
+			full: blog
+		}
+	}
 
-  useEffect(() => {
-    const loggedUserJSON = window.localStorage.getItem('loggedUser')
-    if (loggedUserJSON) {
-      const user = JSON.parse(loggedUserJSON)
-      setUser(user)
-      pt05blogSer.setToken(user.token)
-      dataOfLogged()
-    }
-  }, [])
+	const dataOfLogged = async () => {
+		const serverBlogs = await pt05blogSer.getAll()
+		const blogButtons = await serverBlogs.map(eachButton)
+		const sortedBlogs = blogButtons.sort((a, b) => a.full.likes - b.full.likes)
+		setBlogs(sortedBlogs)
+	}
 
-  const handleLogin = async (event) => {
-    event.preventDefault()
-    
-    try {
-      const user = await loginService.login({
-        username, password,
-      })
+	useEffect(() => {
+		const loggedUserJSON = window.localStorage.getItem('loggedUser')
+		if (loggedUserJSON) {
+			const user = JSON.parse(loggedUserJSON)
+			setUser(user)
+			pt05blogSer.setToken(user.token)
+			dataOfLogged()
+		}
+	}, [])
 
-      //important: note that the term's 'loggedUser' and not something else
-      window.localStorage.setItem(
-        'loggedUser', JSON.stringify(user)
-      ) 
-      pt05blogSer.setToken(user.token)
-      setUser(user)
-      dataOfLogged()
-      setUsername('')
-      setPassword('')
-    } catch (exception) {
-        setNotif("wrong username or password")
-        setTimeout(() => {
-          setNotif('')
-        }, 3000)
-    }
-  }
+	const handleLogin = async (event) => {
+		event.preventDefault()
 
-  function flipVisibility (index) {
-    const varBlogs = [...blogs]
-    varBlogs[index] = {...varBlogs[index],
-      visible: !varBlogs[index].visible
-    }
-    setBlogs(varBlogs)
-  }
+		try {
+			const user = await loginService.login({
+				username, password,
+			})
 
-  function addLike (localBlog) {
-    const newBlog = {...localBlog.full,
-      likes: localBlog.full.likes + 1
-    }
-    pt05blogSer.update(localBlog.full.id, newBlog)
-    const newLocalBlog = {
-      visible: localBlog.visible,
-      full: newBlog
-    }
-    const updatedBlogs = blogs.map(
-      (aBlog) => {
-        return aBlog.full.id === localBlog.full.id ? newLocalBlog : aBlog
-    }
-    )
-    setBlogs(updatedBlogs)
-  } 
+			//important: note that the term's 'loggedUser' and not something else
+			window.localStorage.setItem(
+				'loggedUser', JSON.stringify(user)
+			)
+			pt05blogSer.setToken(user.token)
+			setUser(user)
+			dataOfLogged()
+			setUsername('')
+			setPassword('')
+		} catch (exception) {
+			setNotif("wrong username or password")
+			setTimeout(() => {
+				setNotif('')
+			}, 3000)
+		}
+	}
 
-  function blogRemoval(mongoBlog){
-    if (window.confirm("Remove blog " + mongoBlog.title + " by " + mongoBlog.author)) {
-      pt05blogSer.remove(mongoBlog.id)
-      dataOfLogged()
-    }    
-  }
+	function flipVisibility (index) {
+		const varBlogs = [...blogs]
+		varBlogs[index] = { ...varBlogs[index],
+			visible: !varBlogs[index].visible
+		}
+		setBlogs(varBlogs)
+	}
 
-  function removeButton(mongoBlog){
-    for (let userAdded of userAddedBlogs) {
-      const userTitleAuthor = userAdded.title + userAdded.author
-      const mongoTitleAuthor = mongoBlog.title + mongoBlog.author
-      if (userTitleAuthor === mongoTitleAuthor) {
-        return <div>
-          <button onClick={() => blogRemoval(mongoBlog)}>
-            remove
-          </button>
-        </div>
-      }
-    }
-    return
-  }
+	function addLike (localBlog) {
+		const newBlog = { ...localBlog.full,
+			likes: localBlog.full.likes + 1
+		}
+		pt05blogSer.update(localBlog.full.id, newBlog)
+		const newLocalBlog = {
+			visible: localBlog.visible,
+			full: newBlog
+		}
+		const updatedBlogs = blogs.map(
+			(aBlog) => {
+				return aBlog.full.id === localBlog.full.id ? newLocalBlog : aBlog
+			}
+		)
+		setBlogs(updatedBlogs)
+	}
 
-  const React2Blogs = ({ theBlogs }) => {
-    let readyHtml = []
-    for (let i = 0; i < theBlogs.length; i++) {
-      const fullBlog = theBlogs[i].full
-      if (theBlogs[i].visible === false) {
-        readyHtml.push(
-          <div>
-            {fullBlog.title + " " + fullBlog.author}
-            <button onClick={() => flipVisibility(i)}>
-              view
-            </button>
-          </div>
-        )
-      } else {
-        readyHtml.push(
-          <div style={{border: "1px solid black"}}>
-            {fullBlog.title + " " + fullBlog.author}
-            <button onClick={() => flipVisibility(i)}>
-              hide
-            </button>
-            <br/>
-            {fullBlog.url}
-            <br/>
-            <span>likes&nbsp;</span>
-            {fullBlog.likes}
-            <button onClick={() => addLike(theBlogs[i])}>like</button>
-            <br/>
-            {user.name}
-            {removeButton(fullBlog)}
-          </div>
-        )
-      }
-    }
-    return readyHtml
-  }
+	function blogRemoval(mongoBlog){
+		if (window.confirm("Remove blog " + mongoBlog.title + " by " + mongoBlog.author)) {
+			pt05blogSer.remove(mongoBlog.id)
+			dataOfLogged()
+		}
+	}
 
-  const addBlog = async (blog) => {
+	function removeButton(mongoBlog){
+		for (let userAdded of userAddedBlogs) {
+			const userTitleAuthor = userAdded.title + userAdded.author
+			const mongoTitleAuthor = mongoBlog.title + mongoBlog.author
+			if (userTitleAuthor === mongoTitleAuthor) {
+				return <div>
+					<button onClick={() => blogRemoval(mongoBlog)}>
+						remove
+					</button>
+				</div>
+			}
+		}
+		return
+	}
 
-    appRef.current.toggleVisibility()
-  
-    await pt05blogSer.create(blog)
+	const React2Blogs = ({ theBlogs }) => {
+		let readyHtml = []
+		for (let i = 0; i < theBlogs.length; i++) {
+			const fullBlog = theBlogs[i].full
+			if (theBlogs[i].visible === false) {
+				readyHtml.push(
+					<div>
+						{fullBlog.title + " " + fullBlog.author}
+						<button onClick={() => flipVisibility(i)}>
+							view
+						</button>
+					</div>
+				)
+			} else {
+				readyHtml.push(
+					<div style={{ border: "1px solid black" }}>
+						{fullBlog.title + " " + fullBlog.author}
+						<button onClick={() => flipVisibility(i)}>
+							hide
+						</button>
+						<br/>
+						{fullBlog.url}
+						<br/>
+						<span>likes&nbsp;</span>
+						{fullBlog.likes}
+						<button onClick={() => addLike(theBlogs[i])}>like</button>
+						<br/>
+						{user.name}
+						{removeButton(fullBlog)}
+					</div>
+				)
+			}
+		}
+		return readyHtml
+	}
 
-    dataOfLogged()
+	const addBlog = async (blog) => {
 
-    setUsAdBlogs(userAddedBlogs => userAddedBlogs.concat(blog))
+		appRef.current.toggleVisibility()
 
-    setNotif("a new blog " + blog.title + " by " + blog.author + " added")
-    setTimeout(() => {
-      setNotif('')
-    }, 3000)
-  }
+		await pt05blogSer.create(blog)
+
+		dataOfLogged()
+
+		setUsAdBlogs(userAddedBlogs => userAddedBlogs.concat(blog))
+
+		setNotif("a new blog " + blog.title + " by " + blog.author + " added")
+		setTimeout(() => {
+			setNotif('')
+		}, 3000)
+	}
 
 
-  const loginForm = () => {
-    return (
-      <div>
-        <h2>log into application</h2>
-        <Notification message={notification} />
-        <form onSubmit={handleLogin}>
-          <div>
-            username
-              <input
-              type="text"
-              value={username}
-              name="Username"
-              onChange={({ target }) => setUsername(target.value)}
-            />
-          </div>
-          <div>
-            password
-              <input
-              type="password"
-              value={password}
-              name="Password"
-              onChange={({ target }) => setPassword(target.value)}
-            />
-          </div>
-          <button type="submit">login</button>
-        </form>
-      </div>
-    )
-  }
+	const loginForm = () => {
+		return (
+			<div>
+				<h2>log into application</h2>
+				<Notification message={notification} />
+				<form onSubmit={handleLogin}>
+					<div>
+						username
+						<input
+							type="text"
+							value={username}
+							name="Username"
+							onChange={({ target }) => setUsername(target.value)}
+						/>
+					</div>
+					<div>
+						password
+						<input
+							type="password"
+							value={password}
+							name="Password"
+							onChange={({ target }) => setPassword(target.value)}
+						/>
+					</div>
+					<button type="submit">login</button>
+				</form>
+			</div>
+		)
+	}
 
-  const blogView = () => {
-    return (
-      <div>
-        <h1>blogs</h1>
-        <Notification message={notification} />
-        <span>{user.name} logged in</span>
-        <button onClick={logOut}>logout</button>
-        <p></p>
-        <p></p>
-        <p></p>
-        <Togglable buttonLabel="new blog" ref={appRef}>
-          <BlogForm addBlog={addBlog}/>
-        </Togglable>
-        <React2Blogs theBlogs={blogs}/>
-      </div>
-    )
-  }
+	const blogView = () => {
+		return (
+			<div>
+				<h1>blogs</h1>
+				<Notification message={notification} />
+				<span>{user.name} logged in</span>
+				<button onClick={logOut}>logout</button>
+				<p></p>
+				<p></p>
+				<p></p>
+				<Togglable buttonLabel="new blog" ref={appRef}>
+					<BlogForm addBlog={addBlog}/>
+				</Togglable>
+				<React2Blogs theBlogs={blogs}/>
+			</div>
+		)
+	}
 
-  return (
-    <div>
-      {user === null ? loginForm() : blogView()}
-    </div>
-  )  
+	return (
+		<div>
+			{user === null ? loginForm() : blogView()}
+		</div>
+	)
 }
 
 export default App
--- frontend/src/services/pt05blogSer.js
+++ frontend/src/services/pt05blogSer.js
@@ -6,39 +6,39 @@ const baseUrl = 'http://localhost:3001/api/blogs'
 let token = null
 
 const setToken = newToken => {
-  token = `Bearer ${newToken}`
+	token = `Bearer ${newToken}`
 }
 
 const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
+	const request = axios.get(baseUrl)
+	return request.then(response => response.data)
 }
 
 
 const create = async newObject => {
-  const config = {
-    headers: { Authorization: token },
-  }
+	const config = {
+		headers: { Authorization: token },
+	}
 
 
-  const response = await axios.post(baseUrl, newObject, config)
-  return response.data
+	const response = await axios.post(baseUrl, newObject, config)
+	return response.data
 }
 
 const update = async (id, newObject) => {
-  const config = {
-    headers: { Authorization: token },
-  }
+	const config = {
+		headers: { Authorization: token },
+	}
 
-  await axios.put(`${ baseUrl }/${id}`, newObject, config)
+	await axios.put(`${ baseUrl }/${id}`, newObject, config)
 }
 
 const remove = async (id) => {
-  const config = {
-    headers: { Authorization: token }
-  }
+	const config = {
+		headers: { Authorization: token }
+	}
 
-  await axios.delete(`${ baseUrl }/${id}`, config)
+	await axios.delete(`${ baseUrl }/${id}`, config)
 }
 
 export default { getAll, setToken, create, update, remove }
