a9f88328a64b27c9ef8cfcbe399456c476592aad
ArttuBrotherus <44648394+ArttuBrotherus@users.noreply.github.com>
2023-12-29 19:22:48 +0200

    Reformatting white-spaces

--- frontend/src/components/blogViewSepa.js
+++ frontend/src/components/blogViewSepa.js
@@ -2,51 +2,51 @@ import { useState } from 'react'
 import pt05blogSer from '../services/pt05blogSer'
 
 const BlogForm = ({ addBlog }) => {
-  const [newTitle, setTitle] = useState('')
-  const [newAuthor, setAuthor] = useState('')
-  const [newUrl, setUrl] = useState('')
+	const [newTitle, setTitle] = useState('')
+	const [newAuthor, setAuthor] = useState('')
+	const [newUrl, setUrl] = useState('')
 
-  const newBlog = (event) => {
-    event.preventDefault()
-    addBlog({
-      title: newTitle,
-      author: newAuthor,
-      url: newUrl,
-      likes: 0
-    })
+	const newBlog = (event) => {
+		event.preventDefault()
+		addBlog({
+			title: newTitle,
+			author: newAuthor,
+			url: newUrl,
+			likes: 0
+		})
 
-    setTitle('')
-    setAuthor('')
-    setUrl('')
-  }
+		setTitle('')
+		setAuthor('')
+		setUrl('')
+	}
 
-  return (
-    <div>
-      <h2>create new</h2>
+	return (
+		<div>
+			<h2>create new</h2>
 
-      <form onSubmit={newBlog}>
-        <p>title:
-        <input
-          value={newTitle}
-          onChange={event => setTitle(event.target.value)}
-        />
-        </p>
-        <p>author:
-        <input
-          value={newAuthor}
-          onChange={event => setAuthor(event.target.value)}
-        />
-        </p>
-        <p>url:
-        <input
-          value={newUrl}
-          onChange={event => setUrl(event.target.value)}
-        />
-        </p>
-        <button type="submit">create</button>
-      </form>
-    </div>
-  )
+			<form onSubmit={newBlog}>
+				<p>title:
+					<input
+						value={newTitle}
+						onChange={event => setTitle(event.target.value)}
+					/>
+				</p>
+				<p>author:
+					<input
+						value={newAuthor}
+						onChange={event => setAuthor(event.target.value)}
+					/>
+				</p>
+				<p>url:
+					<input
+						value={newUrl}
+						onChange={event => setUrl(event.target.value)}
+					/>
+				</p>
+				<button type="submit">create</button>
+			</form>
+		</div>
+	)
 }
 
 export default BlogForm
