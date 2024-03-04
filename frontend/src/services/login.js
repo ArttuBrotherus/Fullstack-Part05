d732e84f6b15dfa00ac0b29afb31e22c8032c622
ArttuBrotherus <44648394+ArttuBrotherus@users.noreply.github.com>
2024-03-04 12:23:26 +0200

    Cypress configuration

--- frontend/src/services/login.js
+++ frontend/src/services/login.js
@@ -3,8 +3,8 @@ const baseUrl = 'http://localhost:3001/api/login'
 //you may need to specify the baseUrl in other services in the future just like here
 
 const login = async credentials => {
-  const response = await axios.post(baseUrl, credentials)
-  return response.data
+	const response = await axios.post(baseUrl, credentials)
+	return response.data
 }
 
 const stepLogin = { login: login }
